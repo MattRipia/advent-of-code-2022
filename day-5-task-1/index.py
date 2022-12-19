64856542227640dc1c3cf502d05fb0a25661938c
Matt Ripia <matt.ripia@mattr.global>
2022-12-19 13:24:51 +1300

    completed day 5 part 1

--- day-5-task-1/index.py
+++ day-5-task-1/index.py
@@ -1,3 +1,16 @@
+import re
+
+def move_char(stack_dict: dict, from_stack: int, to_stack: int):
+    print("Before Move:")
+    for key in stack_dict.keys():
+        print(key, stack_dict[key])
+
+    stack_dict[to_stack].append(stack_dict[from_stack].pop())
+
+    print("After Move:")
+    for key in stack_dict.keys():
+        print(key, stack_dict[key])
+
 
 #     [D]    
 # [N] [C]    
@@ -9,36 +22,57 @@
 # move 2 from 2 to 1
 # move 1 from 1 to 2
 
-with open('input.txt', 'r') as f:
-    lines = f.readlines()
-    lines = [entry for entry in lines]
+with open('input.txt', 'r') as infile:
+    lines = [line for line in infile]
 
-initial_dict = {}
-for count in range(1, 10):
-    initial_dict[count] = list()
-print(initial_dict)
-
+# read input into 3 lists depending on the new line
+line_no = 0
 for line in lines:
-    print(line)
-    count = 0;
+    if line == "\n":
+        stacks = lines[:line_no - 1]
+        instructions = lines[line_no + 1:]
+        num_stacks = lines[line_no - 1]
+    line_no += 1
+
+# find the number of stacks from input (1   2   3) -> get last index convert to int
+num_stacks = int(num_stacks.strip().split("   ")[-1])
+
+# create stacks
+stack_dict = {}
+for count in range(1, num_stacks + 1):
+    stack_dict[count] = list()
+# print(stack_dict)
+
+# Divide up each line from 'stacks' and find the stack to dump the character
+for line in stacks:
+    count = 0
     for char in line:
-        print(count,"(",char,")",(int(count / 4) + 1))
+        bucket_no = int(count / 4) + 1
         if char.isalpha():
-            initial_dict[int(count / 4) + 1].append(char)
+            stack_dict[bucket_no].append(char)
         count+=1
-        
-print(initial_dict)
 
 # reverse to get the correct stack
-for key in initial_dict.keys():
-    initial_dict[key].reverse()
-    print(initial_dict[key])
+for key in stack_dict.keys():
+    stack_dict[key].reverse()
+    print(key, stack_dict[key])
 
-initial_dict[1].pop()
-print(initial_dict[1])
+# perform the stack swapping
+for instruction in instructions:
+    stripped = str(instruction.strip())
+    _frag_instructions = re.findall(r'\d+', stripped)
+    print("Moves:", _frag_instructions[0])
+    print("From:", _frag_instructions[1])
+    print("To:", _frag_instructions[2])
 
-# do the stack operations  
-#      
-# 1 = 1
-# 5 = 2
-# 9 = 3
+    # move from -> to, num times
+    for num in range(1, int(_frag_instructions[0]) + 1):
+        print(num)
+        move_char(stack_dict, int(_frag_instructions[1]), int(_frag_instructions[2]))
+
+chars = ''
+for key in stack_dict.keys():
+    char = stack_dict[key][-1]
+    chars += char
+
+print(chars)
